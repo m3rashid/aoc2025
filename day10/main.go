8ea85a420c1186391f5efea665acd9b4d2957033
m3rashid <m3rashid.hussain@gmail.com>
2025-12-19 02:14:08 +0530

    feat: day10

--- day10/main.go
+++ day10/main.go
@@ -1,98 +1,294 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"math"
 	"strconv"
 	"strings"
 )
 
-type Data struct {
-	bulbPattern      map[int]bool // position -> bulb is on
-	wiringSchemantic [][]int
-	joltages         []int
+type Puzzle struct {
+	A     [][]bool
+	b     []bool
+	jolts []int
 }
 
-func parseData(_str string) []Data {
-	lines := strings.Split(_str, "\n")
-	data := []Data{}
+func createPuzzle(parts []string) Puzzle {
+	newPuzzle := Puzzle{}
 
-	for _, line := range lines {
-		pos := 1 // 0 is [
-		for line[pos] != ']' {
-			pos++
+	// [...]
+	innerBrackets := parts[0][1 : len(parts[0])-1]
+	for i := 0; i < len(innerBrackets); i++ {
+		newPuzzle.b = append(newPuzzle.b, innerBrackets[i] == '#')
+	}
+
+	// (...)
+	rows := len(newPuzzle.b)
+	cols := len(parts) - 2
+	var A [][]bool
+	for range rows {
+		var row []bool
+		for range cols {
+			row = append(row, false)
 		}
+		A = append(A, row)
+	}
+	for j := 1; j < len(parts)-1; j++ {
+		innerBrackets := parts[j][1 : len(parts[j])-1]
+		numbers := strings.SplitSeq(innerBrackets, ",")
 
-		bulbPatternStr := strings.TrimSpace(line[1:pos])
-		bulbPattern := map[int]bool{}
-		for i, ch := range bulbPatternStr {
-			switch ch {
-			case '#':
-				bulbPattern[i] = true
-			case '.':
-				bulbPattern[i] = false
+		for n := range numbers {
+			i, _ := strconv.Atoi(n)
+			A[i][j-1] = true
+		}
+	}
+	newPuzzle.A = A
+
+	// {...}
+	innerBrackets = parts[len(parts)-1][1 : len(parts[len(parts)-1])-1]
+	jolts := strings.SplitSeq(innerBrackets, ",")
+	for j := range jolts {
+		i, _ := strconv.Atoi(j)
+		newPuzzle.jolts = append(newPuzzle.jolts, i)
+	}
+
+	return newPuzzle
+}
+
+func readInput(_data string) []Puzzle {
+	var puzzles []Puzzle
+	lines := strings.SplitSeq(_data, "\n")
+
+	for line := range lines {
+		parts := strings.Split(line, " ")
+		newPuzzle := createPuzzle(parts)
+		puzzles = append(puzzles, newPuzzle)
+	}
+
+	return puzzles
+}
+
+func increase(x []bool) {
+	i := len(x) - 1
+	propagation := true
+
+	for propagation && i >= 0 {
+		propagation = x[i]
+		x[i] = !x[i]
+		i--
+	}
+}
+
+func check(A [][]bool, b []bool, x []bool) bool {
+	rows := len(A)
+	cols := len(A[0])
+
+	for i := range rows {
+		trueCounts := 0
+		for j := range cols {
+			if A[i][j] && x[j] {
+				trueCounts++
+			}
+		}
+		if (trueCounts%2 == 1) != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func countTrues(a []bool) int {
+	n := 0
+	for _, i := range a {
+		if i {
+			n++
+		}
+	}
+	return n
+}
+
+func solve(A [][]bool, b []bool) int {
+	x := make([]bool, len(A[0]))
+	for i := range x {
+		x[i] = false
+	}
+
+	minCount := math.MaxInt
+	maxCombinations := int(math.Pow(2, float64(len(x))))
+
+	for range maxCombinations {
+		if check(A, b, x) {
+			count := countTrues(x)
+			if count < minCount {
+				minCount = count
+			}
+		}
+		increase(x)
+	}
+
+	return minCount
+}
+
+type Var struct {
+	pos int
+	val float64
+}
+
+func buildAugmentedMatrix(A [][]bool, b []int) [][]float64 {
+	m := len(A)    // number of rows
+	n := len(A[0]) // number of columns (variables)
+
+	if len(b) != m {
+		panic("row count of A does not match length of b")
+	}
+
+	aug := make([][]float64, m)
+
+	for i := range m {
+		row := make([]float64, n+1)
+
+		for j := range n {
+			if A[i][j] {
+				row[j] = 1
+			} else {
+				row[j] = 0
 			}
 		}
 
-		pos += 2 // skip ] ans space
-		initialPos := pos
-		for line[pos] != '{' {
-			pos++
-		}
-		buttonWiringSchemanticStr := strings.TrimSpace(line[initialPos:pos])
-		schemas := strings.Split(buttonWiringSchemanticStr, " ")
+		row[n] = float64(b[i]) // RHS
+		aug[i] = row
+	}
 
-		wiringSchemantic := [][]int{}
-		for _, sc := range schemas {
-			schema := sc[1 : len(sc)-1] // remove the ( and )
+	return aug
+}
 
-			positions := strings.Split(schema, ",")
-			positionsInt := []int{}
-			for _, pos := range positions {
-				posInt, _ := strconv.Atoi(pos)
-				positionsInt = append(positionsInt, posInt)
+func getFreeVariables(A [][]float64) []Var {
+	var freeVars []Var
+
+	m := len(A)
+	n := len(A[0]) - 1 // exclude RHS
+
+	pivotCols := make(map[int]bool)
+
+	for row := range m {
+		for col := range n {
+			if A[row][col] != 0 {
+				pivotCols[col] = true
+				break
 			}
-			wiringSchemantic = append(wiringSchemantic, positionsInt)
 		}
-
-		pos += 1 // skip } and space
-		initialPos = pos
-		for line[pos] != '}' {
-			pos++
-		}
-		joltageRequirementsStr := strings.TrimSpace(line[initialPos:pos])
-		requirements := strings.Split(joltageRequirementsStr, ",")
-		joltageRequirements := []int{}
-		for _, req := range requirements {
-			reqInt, _ := strconv.Atoi(req)
-			joltageRequirements = append(joltageRequirements, reqInt)
-		}
-
-		data = append(data, Data{bulbPattern: bulbPattern, wiringSchemantic: wiringSchemantic, joltages: joltageRequirements})
 	}
 
-	return data
-}
-
-func analyzePart1(data Data) int {
-	return 0
-}
-
-func part1() {
-	inputData := parseData(sample)
-	totalClicks := 0
-
-	for _, data := range inputData {
-		totalClicks += analyzePart1(data)
+	for col := range n {
+		if _, ok := pivotCols[col]; !ok {
+			freeVars = append(freeVars, Var{pos: col, val: 0})
+		}
 	}
 
-	fmt.Println("Part 1 total: ", totalClicks)
+	return freeVars
 }
 
-func part2() {
+func extractSolution(A [][]float64, free []Var) []float64 {
+	n := len(A[0]) - 1
+	x := make([]float64, n)
 
+	for _, v := range free {
+		x[v.pos] = v.val
+	}
+
+	for i := range A {
+		p := -1
+		for j := range n {
+			if math.Abs(A[i][j]-1) < eps {
+				p = j
+				break
+			}
+		}
+		if p == -1 {
+			continue
+		}
+		val := A[i][n]
+		for j := range n {
+			if j != p {
+				val -= A[i][j] * x[j]
+			}
+		}
+		x[p] = val
+	}
+	return x
 }
+
+func search(A [][]float64, level int, free []Var) {
+	if level == len(free) {
+		x := extractSolution(A, free)
+		sum := 0
+		for _, v := range x {
+			iv := math.Round(v)
+
+			if math.Abs(v-iv) > eps {
+				return // not integer
+			}
+
+			if iv < 0 {
+				return // truly negative
+			}
+
+			sum += int(iv)
+		}
+
+		if sum < bestSum {
+			bestSum = sum
+			bestSolution = append([]float64{}, x...)
+		}
+		return
+	}
+
+	for v := 0; v <= MAX; v++ {
+		next := make([]Var, len(free))
+		copy(next, free)
+		next[level].val = float64(v)
+		search(A, level+1, next)
+	}
+}
+
+func getHighestJolt(jolts []int) int {
+	highest := jolts[0]
+	for _, j := range jolts {
+		absJ := int(math.Abs(float64(j)))
+		if absJ > highest {
+			highest = absJ
+		}
+	}
+	return highest
+}
+
+var MAX int
+var bestSum int
+var bestSolution []float64
 
 func main() {
-	part1()
-	part2()
+	puzzles := readInput(data)
+	minCounts := 0
+	for _, puzzle := range puzzles {
+		minCount := solve(puzzle.A, puzzle.b)
+		minCounts += minCount
+	}
+	log.Println("PART 1::", minCounts)
+
+	total := 0
+	for _, puzzle := range puzzles {
+		MAX = getHighestJolt(puzzle.jolts)
+		bestSum = math.MaxInt
+		bestSolution = make([]float64, 0)
+
+		augmented := buildAugmentedMatrix(puzzle.A, puzzle.jolts)
+		gA := rref(augmented)
+		free := getFreeVariables(gA)
+		search(gA, 0, free)
+
+		total += int(bestSum)
+	}
+
+	log.Println("PART 2::", total)
 }
