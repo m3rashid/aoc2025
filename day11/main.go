16b83a3a1272498cc86a813a5e27bedb3a296e0f
m3rashid <m3rashid.hussain@gmail.com>
2025-12-19 02:19:00 +0530

    feat: day11

--- day11/main.go
+++ day11/main.go
@@ -5,10 +5,12 @@ import (
 	"strings"
 )
 
-func parseData(_data string) map[string][]string {
+type Graph map[string][]string
+
+func parseData(_data string) Graph {
 	lines := strings.Split(_data, "\n")
 
-	data := map[string][]string{}
+	data := Graph{}
 	for _, line := range lines {
 		parts := strings.Split(line, ":")
 		source := strings.TrimSpace(parts[0])
@@ -19,44 +21,68 @@ func parseData(_data string) map[string][]string {
 	return data
 }
 
-var sampleInput = parseData(sample2)
-
-func countPart1(current string, val int, visited map[string]bool) int {
-	fmt.Println("Current: ", current, "Value: ", val)
-
-	visited[current] = true
-	for _, other := range sampleInput[current] {
-		if other == "out" {
-			val += 1
-		} else {
-			val = countPart1(other, val, visited)
-		}
+func countPathsDFS(graph Graph, src, dst string, memo map[string]int) int {
+	if src == dst {
+		return 1
 	}
-	return val
+
+	if v, ok := memo[src]; ok {
+		return v
+	}
+
+	total := 0
+	for _, next := range graph[src] {
+		total += countPathsDFS(graph, next, dst, memo)
+	}
+
+	memo[src] = total
+	return total
 }
 
-func part1() {
-	visited := map[string]bool{}
-	res := countPart1("you", 0, visited)
-	fmt.Println("Part 1: ", res)
+func hasPath(graph Graph, src, dst string, visited map[string]bool) bool {
+	if src == dst {
+		return true
+	}
+
+	if visited[src] {
+		return false
+	}
+	visited[src] = true
+
+	for _, next := range graph[src] {
+		if hasPath(graph, next, dst, visited) {
+			return true
+		}
+	}
+
+	return false
 }
 
-func part2() {
-	stack := Stack[string]{}
-	stack.Push("svr")
+func countPaths(graph Graph, src, dst string) int {
+	return countPathsDFS(graph, src, dst, make(map[string]int))
+}
 
-	paths := map[string][]string{}
-
-	for !stack.IsEmpty() {
-		current, _ := stack.Pop()
-		if _, ok := paths[current]; !ok {
-			paths[current] = []string{}
-		}
-
+func countPathsVia(graph Graph, src, dst string, viaA, viaB string) int {
+	// Determine order
+	if hasPath(graph, viaA, viaB, make(map[string]bool)) {
+		return pathsThrough(graph, src, viaA, viaB, dst)
 	}
+	return pathsThrough(graph, src, viaB, viaA, dst)
+}
+
+func pathsThrough(graph Graph, src, mid1, mid2, dst string) int {
+	p1 := countPathsDFS(graph, src, mid1, make(map[string]int))
+	p2 := countPathsDFS(graph, mid1, mid2, make(map[string]int))
+	p3 := countPathsDFS(graph, mid2, dst, make(map[string]int))
+	return p1 * p2 * p3
 }
 
 func main() {
-	part1()
-	part2()
+	graph := parseData(data)
+
+	part1 := countPaths(graph, "you", "out")
+	fmt.Printf("Part 1: %d\n", part1)
+
+	part2 := countPathsVia(graph, "svr", "out", "dac", "fft")
+	fmt.Printf("Part 2: %d\n", part2)
 }
